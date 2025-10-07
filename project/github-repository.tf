9dc3d214740e26cea3a30e346fc03980e1b85d97
LarsGJobloop <lars.gunnar@jobloop.no>
2025-10-07 13:34:50 +0200

    setup github repository

---
+++ project/github-repository.tf
@@ -0,0 +1,11 @@
+resource "github_repository" "github_repository" {
+  name = "2025-10-subsidiary-pattern"
+  description = "Example repository for the subsidiary pattern"
+  visibility = "private"
+}
+
+output "github_repository_url" {
+  description = "The URL of the GitHub repository"
+  value = github_repository.github_repository.http_clone_url
+  sensitive = false
+}
