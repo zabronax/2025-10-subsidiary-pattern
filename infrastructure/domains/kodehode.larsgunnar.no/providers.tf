78b741d219d7333f68139341fcf4afb7260734d7
zabronax <zabronax@gmail.com>
2025-10-08 02:53:53 +0200

    Update Terraform backend key for kodehode.larsgunnar.no

--- infrastructure/domains/kodehode.larsgunnar.no/providers.tf
+++ infrastructure/domains/kodehode.larsgunnar.no/providers.tf
@@ -9,7 +9,7 @@ terraform {
   # This is Hetzner's Ceph Object Storage
   backend "s3" {
     bucket = "zabronax-state"
-    key    = "larsgunnar.no/terraform.tfstate"
+    key    = "kodehode.larsgunnar.no/terraform.tfstate"
 
     skip_region_validation      = true
     skip_credentials_validation = true
