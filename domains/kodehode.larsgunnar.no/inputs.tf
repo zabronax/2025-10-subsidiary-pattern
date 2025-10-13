f80e36c22fb857d839ad304d5ee5c1d35cbd25e5
zabronax <zabronax@gmail.com>
2025-10-13 15:13:18 +0200

    clear out iac for kodehode.larsgunnar.no

--- domains/kodehode.larsgunnar.no/inputs.tf
+++
@@ -1,5 +0,0 @@
-variable "cloudflare_account_id" {
-  description = "Cloudflare account ID for the account hosting the larsgunnar.no domain"
-  type        = string
-  sensitive   = true # Uncertain if this is sensitive, but it's a variable so we'll keep it that way
-}
