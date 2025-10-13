1a1eb3d8338c790a8a12a74cf4bbad1002b17031
zabronax <zabronax@gmail.com>
2025-10-13 19:34:28 +0200

    Add kodehode.larsgunnar.no as a delegated subdomain

--- domains/larsgunnar.no/records-delegated.tf
+++ domains/larsgunnar.no/records-delegated.tf
@@ -1,16 +1,28 @@
-locals {
+module "delegated_domain" {
+  source             = "./modules/delegated-domain"
+  cloudflare_zone_id = cloudflare_zone.apex.id
+
+  ttl = local.ttl
+
+  # Delegated domain configuration
+  fqdn = "kodehode.${local.domain_name}"
   desec_name_servers = [
     "ns1.desec.io",
     "ns2.desec.org",
   ]
-}
 
-resource "cloudflare_dns_record" "kodehode_larsgunnar_no" {
-  for_each = toset(local.desec_name_servers)
-
-  zone_id = cloudflare_zone.apex.id
-  name    = "kodehode"
-  type    = "NS"
-  content = each.value
-  ttl     = local.ttl
+  dnssec_ds_records = [
+    {
+      key_tag     = "32103"
+      digest      = "C61C16FD5F457047BFA7CBF94BDB58D754FBA55C3DF9F516C79018E96C9EE1AF"
+      algorithm   = 13
+      digest_type = 2
+    },
+    {
+      key_tag     = "32103"
+      digest      = "C468693C14F304F988AE93335EC2131EEACB26D6AABA2091130B141E5819FB90BFC1337DD59124487C834815265598AE"
+      algorithm   = 13
+      digest_type = 4
+    }
+  ]
 }
