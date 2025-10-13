f29730bc95fe81e9730903f2cd37391358fe8fb0
zabronax <zabronax@gmail.com>
2025-10-13 17:09:01 +0200

    Refactor: update domain configuration for kodehode.larsgunnar.no

--- domains/kodehode.larsgunnar.no/main.tf
+++ domains/kodehode.larsgunnar.no/main.tf
@@ -1,35 +1,29 @@
 locals {
-  domain_name = "kodehode.larsgunnar.no"
-  ttl         = 3600 # 1 hour
+  parent_domain_name = "larsgunnar.no"
+  domain_name        = "kodehode"
+  fqdn               = "${local.domain_name}.${local.parent_domain_name}"
+  ttl                = 3600 # 1 hour
+}
 
+resource "desec_domain" "kodehode" {
+  name = local.fqdn
+}
+
+resource "desec_rrset" "kodehode_record_a" {
+  domain = local.fqdn
+  ttl    = local.ttl
+
+  subname = "" # Apex domain
+  type    = "A"
   records = [
-    {
-      type = "A"
-      records = [
-        "37.27.246.167",
-      ]
-    }
+    "37.27.246.167",
   ]
+
+  depends_on = [desec_domain.kodehode]
 }
 
-resource "desec_domain" "kodehode_larsgunnar_no" {
-  name = local.domain_name
-}
-
-resource "desec_rrset" "kodehode_larsgunnar_no_records" {
-  for_each = { for record in local.records : record.type => record }
-
-  # Static values
-  domain  = local.domain_name
-  subname = "" # Apex record
-  ttl     = local.ttl
-
-  # Dynamic values
-  type    = each.key
-  records = each.value.records
-}
-
-output "kodehode_larsgunnar_no_records" {
-  description = "The records for the kodehode.larsgunnar.no domain"
-  value       = desec_rrset.kodehode_larsgunnar_no_records
+output "kodehode_record_a" {
+  description = "The A record for the kodehode.larsgunnar.no domain"
+  value       = desec_rrset.kodehode_record_a
+  sensitive   = false
 }
