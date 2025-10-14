5c820cf584619949b87c14775ab943294c0720ed
zabronax <zabronax@gmail.com>
2025-10-14 12:24:01 +0200

    Add CNAME record for test web app in root DNS zone

--- domains/larsgunnar.no/records-delegated.tf
+++ domains/larsgunnar.no/records-delegated.tf
@@ -27,3 +27,11 @@ module "delegated_domain" {
     }
   ]
 }
+
+resource "cloudflare_dns_record" "app_cname" {
+  zone_id = cloudflare_zone.apex.id
+  name    = "app"
+  type    = "CNAME"
+  content = "cname.vercel-dns.com"
+  ttl     = local.ttl
+}
