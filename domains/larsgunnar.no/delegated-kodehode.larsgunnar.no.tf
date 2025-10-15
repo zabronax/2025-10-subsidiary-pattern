1ed25229874edc96180429d47d58aa872f431ca5
zabronax <zabronax@gmail.com>
2025-10-15 09:17:10 +0200

    Refactor configuration for delegated subdomains

--- domains/larsgunnar.no/delegated-kodehode.larsgunnar.no.tf
+++ domains/larsgunnar.no/delegated-kodehode.larsgunnar.no.tf
@@ -27,11 +27,3 @@ module "delegated_domain" {
     }
   ]
 }
-
-resource "cloudflare_dns_record" "app_cname" {
-  zone_id = cloudflare_zone.apex.id
-  name    = "app"
-  type    = "CNAME"
-  content = "cname.vercel-dns.com"
-  ttl     = local.ttl
-}
