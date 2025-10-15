6e096475f356ddee9c351844bf513997de86c068
zabronax <zabronax@gmail.com>
2025-10-15 11:13:52 +0200

    Update DNS records for larsgunnar.no to use new server IPs
    - Changed local variable for server IPv4 address and added IPv6 address.
    - Updated A records for www and apex domains to reflect the new IPv4 address.
    - Introduced a new AAAA record for the apex domain to support IPv6.

--- domains/larsgunnar.no/domain-apex.tf
+++ domains/larsgunnar.no/domain-apex.tf
@@ -1,11 +1,12 @@
 locals {
-  larsgunnar_server_ip = "46.62.222.201"
+  larsgunnar_server_ipv4 = "65.109.240.156"
+  larsgunnar_server_ipv6 = "2a01:4f9:c01f:49::"
 }
 
 resource "cloudflare_dns_record" "www" {
   zone_id = cloudflare_zone.apex.id
   name    = "www"
-  content = local.larsgunnar_server_ip
+  content = local.larsgunnar_server_ipv4
   type    = "A"
   ttl     = local.ttl
   proxied = false
@@ -15,7 +16,16 @@ resource "cloudflare_dns_record" "a_record" {
   zone_id = cloudflare_zone.apex.id
   name    = "@"
   type    = "A"
-  content = local.larsgunnar_server_ip
+  content = local.larsgunnar_server_ipv4
+  ttl     = local.ttl
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "aaaa_record" {
+  zone_id = cloudflare_zone.apex.id
+  name    = "@"
+  type    = "AAAA"
+  content = local.larsgunnar_server_ipv6
   ttl     = local.ttl
   proxied = false
 }
