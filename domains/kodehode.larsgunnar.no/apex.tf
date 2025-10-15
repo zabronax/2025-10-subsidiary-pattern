07eca03736a449d714244a50e64e09c2b5db9298
zabronax <zabronax@gmail.com>
2025-10-15 10:12:15 +0200

    Add IPv4 and IPv6 DNS records for kodehode.larsgunnar.no
    - Introduced local variables for server IPv4 and IPv6 addresses.
    - Added A and AAAA record resources for both apex and wildcard domains.
    - Updated outputs to include the new AAAA record information.

--- domains/kodehode.larsgunnar.no/apex.tf
+++ domains/kodehode.larsgunnar.no/apex.tf
@@ -3,6 +3,9 @@ locals {
   domain_name        = "kodehode"
   fqdn               = "${local.domain_name}.${local.parent_domain_name}"
   ttl                = 3600 # 1 hour
+
+  server_ipv4 = "65.109.240.156"
+  server_ipv6 = "2a01:4f9:c01f:49::"
 }
 
 resource "desec_domain" "kodehode" {
@@ -17,6 +20,10 @@ output "kodehode_dnssec" {
   }
 }
 
+# ================
+# = IPv4 Records =
+# ================
+
 resource "desec_rrset" "kodehode_record_a" {
   domain = local.fqdn
   ttl    = local.ttl
@@ -24,7 +31,7 @@ resource "desec_rrset" "kodehode_record_a" {
   subname = "" # Apex domain
   type    = "A"
   records = [
-    "37.27.246.167",
+    local.server_ipv4,
   ]
 
   depends_on = [desec_domain.kodehode]
@@ -37,7 +44,7 @@ resource "desec_rrset" "kodehode_record_a_wildcard" {
   subname = "*" # Wildcard domain
   type    = "A"
   records = [
-    "37.27.246.167",
+    local.server_ipv4,
   ]
 
   depends_on = [desec_domain.kodehode]
@@ -52,5 +59,45 @@ output "kodehode_record_a" {
 output "kodehode_record_a_wildcard" {
   description = "The A record for the kodehode.larsgunnar.no domain"
   value       = desec_rrset.kodehode_record_a_wildcard
+  sensitive   = false
+}
+
+# ================
+# = IPv6 Records =
+# ================
+
+resource "desec_rrset" "kodehode_record_aaaa" {
+  domain = local.fqdn
+  ttl    = local.ttl
+
+  subname = "" # Apex domain
+  type    = "AAAA"
+  records = [
+    local.server_ipv6,
+  ]
+
+  depends_on = [desec_domain.kodehode]
+}
+
+resource "desec_rrset" "kodehode_record_aaaa_wildcard" {
+  domain = local.fqdn
+  ttl    = local.ttl
+
+  subname = "*" # Wildcard domain
+  type    = "AAAA"
+  records = [
+    local.server_ipv6,
+  ]
+}
+
+output "kodehode_record_aaaa" {
+  description = "The AAAA record for the kodehode.larsgunnar.no domain"
+  value       = desec_rrset.kodehode_record_aaaa
+  sensitive   = false
+}
+
+output "kodehode_record_aaaa_wildcard" {
+  description = "The AAAA record for the kodehode.larsgunnar.no domain"
+  value       = desec_rrset.kodehode_record_aaaa_wildcard
   sensitive   = false
 }
