709b6446b5e5264a923f6afaa524d2230a449a69
zabronax <zabronax@gmail.com>
2025-10-15 10:03:59 +0200

    Enhance floating IP configuration in for test-infra/server
    - Introduced separate resources for primary IPv4 and IPv6 floating IPs with descriptions.
    - Added local variable for location to improve maintainability.
    - Updated server resource to use the local location variable.
    - Modified output to return both IPv4 and IPv6 addresses of the server.

--- test-infra/server/main.tf
+++ test-infra/server/main.tf
@@ -21,21 +21,47 @@ resource "hcloud_ssh_key" "ssh_key" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5JL1ZtyUaBS7DsyFWdLzaalHDMA0ZExbDchJvOEjon"
 }
 
-resource "hcloud_floating_ip" "primary_ip" {
-  name      = "primary_ip"
-  type      = "ipv4"
-  server_id = hcloud_server.server.id
+locals {
+  location = "hel1"
+}
+
+resource "hcloud_floating_ip" "primary_ipv4" {
+  description   = "Primary IPv4 address"
+  name          = "primary_ip"
+  type          = "ipv4"
+  home_location = local.location
+}
+
+resource "hcloud_floating_ip" "primary_ipv6" {
+  description   = "Primary IPv6 address"
+  name          = "primary_ipv6"
+  type          = "ipv6"
+  home_location = local.location
+}
+
+resource "hcloud_floating_ip_assignment" "primary_ipv4_assignment" {
+  server_id      = hcloud_server.server.id
+  floating_ip_id = hcloud_floating_ip.primary_ipv4.id
+}
+
+resource "hcloud_floating_ip_assignment" "primary_ipv6_assignment" {
+  server_id      = hcloud_server.server.id
+  floating_ip_id = hcloud_floating_ip.primary_ipv6.id
 }
 
 resource "hcloud_server" "server" {
   name        = "server"
   image       = "debian-12"
   server_type = "cx32"
-  location    = "hel1"
+  location    = local.location
   ssh_keys    = [hcloud_ssh_key.ssh_key.id]
   user_data   = module.compose-debian-cloud-init.cloud_init
 }
 
-output "server_ip" {
-  value = hcloud_server.server.ipv4_address
+output "server_addresses" {
+  description = "The IPv4 and IPv6 addresses of the server"
+  value = {
+    ipv4 = hcloud_floating_ip.primary_ipv4.ip_address
+    ipv6 = hcloud_floating_ip.primary_ipv6.ip_address
+  }
 }
